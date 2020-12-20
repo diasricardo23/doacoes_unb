5cf369de4730c2f31eef98aec9e13cbc5c0a5a59
Ricardo Dias <diasricardo@MacBook-Pro-de-Ricardo.local>
2020-12-20 18:19:23 -0300

    First Commit

---
+++ dist/precache-manifest.0fa2ad97c6eeddedafeb19314aae9d9e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab830b6807c3bf12af20",
+    "url": "/css/chunk-vendors.02783f14.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e28d0e140a292ce203881fdd3fa05caa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b220fd4a9e88006bec0",
+    "url": "/js/about.38c77848.js"
+  },
+  {
+    "revision": "148f06918b09137ab6ac",
+    "url": "/js/app.7ff2d212.js"
+  },
+  {
+    "revision": "ab830b6807c3bf12af20",
+    "url": "/js/chunk-vendors.e7aa0556.js"
+  },
+  {
+    "revision": "159fb2106d1874a891c8b14c8e0ae812",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
