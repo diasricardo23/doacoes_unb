fea24e3984334728be4066c488f29381f157ed26
Fernanda Formiga <ferformigas@gmail.com>
2021-02-27 17:51:04 -0300

    Change Home Page and fix About Project

--- src/views/AboutProject.vue
+++ src/views/AboutProject.vue
@@ -1,95 +1,100 @@
 <template>
   <div class="container3">
-    <div class="secondary containerDrawer" >
-        <v-btn to="/" class="backButton">
-            <v-icon>mdi-arrow-left</v-icon>
-        </v-btn>
+    <div class="secondary containerDrawer">
+      <v-btn to="/" class="backButton">
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
     </div>
-    
+
     <div class="containerImg">
-        <img alt="Vue logo" src="../assets/logo.png" />
+      <v-img alt="Logo UnB" src="../assets/logo.png" contain />
     </div>
     <div class="containerAbout">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis 
-        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+      aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </div>
-    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-    data: () => ({
-      drawer: false,
-      group: null,
-    }),
+  data: () => ({
+    drawer: false,
+    group: null
+  }),
 
-    watch: {
-      group () {
-        this.drawer = false
-      },
-    },
+  watch: {
+    group() {
+      this.drawer = false;
+    }
   }
-
+};
 </script>
 
 <style scoped>
-    .container3 {
-        background-color: #1D2D50;
-        display: flex;
-        flex: 1;
-        flex-direction: column;
-        align-items: center;
-        width: 100%;
-    }
+.container3 {
+  background-color: white;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
 
-    .containerDrawer {
-        width: 100%;
-        height: 7%;
-        display: flex;
-        align-items: center;
-        border-bottom-left-radius: 8px;
-        border-bottom-right-radius: 8px;
-    }
+.containerDrawer {
+  width: 100%;
+  height: 7%;
+  display: flex;
+  align-items: center;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
 
-    .containerAbout {
-        width: 100%;
-        text-align: justify;
-        padding: 20px;
-        color: white;
-    }
+.containerAbout {
+  width: 80%;
+  text-align: justify;
+  color: black;
+}
 
-    .containerMenu {
-        width: 85%;
-        display: flex;
-        justify-content: space-between;
-    }
+.containerMenu {
+  width: 85%;
+  display: flex;
+  justify-content: space-between;
+}
 
-    .infobox {
-        background-color: white;
-        color: #485550;
-        padding: 10px;
-        border-radius: 10px;
-        margin-bottom: 20px;
-        margin-top: 20px;
-    }
+.containerImg {
+  width: 60% !important;
+  display: flex;
+  flex: 2;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-    .donations {
-        width: 88%;
-    }
+.infobox {
+  background-color: white;
+  color: #485550;
+  padding: 10px;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
 
-    .menu {
-        width: 45%;
-        text-align: center;
-    }
+.donations {
+  width: 88%;
+}
 
-    .backButton {
-        background-color: transparent !important;
-        color: white !important;
-        box-shadow: none !important;
-    }
+.menu {
+  width: 45%;
+  text-align: center;
+}
 
+.backButton {
+  background-color: transparent !important;
+  color: white !important;
+  box-shadow: none !important;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,71 @@
 <template>
   <div class="container1">
     <div class="containerImg">
-        <v-img alt="Logo UnB" src="../assets/logo.png" max-width="200" max-height="150" contain />
+      <v-img alt="Logo UnB" src="../assets/logo.png" contain />
     </div>
+    <div class="logo-title">DOAÇÕES COVID UNB</div>
     <div class="containerButtons">
-        <v-btn rounded class="secondary white--text loginBtn mb-4" to="/loginadm">Login Administrador</v-btn>
-        <v-btn rounded class="secondary white--text loginBtn mb-4" to="/logindonator">Login Doador</v-btn>
-        <v-btn rounded class="secondary white--text loginBtn" to="/about">Sobre a Ação</v-btn>
+      <v-btn color="primary" class="loginBtn mb-4" to="/loginadm">Login Admin</v-btn>
+      <v-btn color="primary" class="loginBtn mb-4" to="/logindonator">Login Doador</v-btn>
+      <v-btn color="primary" class="loginBtn mb-4" to="/about">Sobre a Ação</v-btn>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
 </script>
 
 <style scoped>
-    .container1 {
-        background-color: rgb(19, 62, 121);
-        display: flex;
-        flex: 1;
-        flex-direction: column;
-        align-items: center;
-        width: 100%;
-    }
+.container1 {
+  background-color: white;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
 
-    .containerImg {
-        width: 100% !important;
-        display: flex;
-        flex: 2;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
+.logo-title {
+  color: #1d2d50;
+  font-size: 18px;
+  font-weight: 500;
+  margin-bottom: 25%;
+}
 
-    .containerButtons {
-        width: 100%;
-        display: flex;
-        flex: 1;
-        flex-direction: column;
-        align-items: center;
-    }
+.containerImg {
+  width: 60% !important;
+  display: flex;
+  flex: 2;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 
-    .containerInput {
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+.containerButtons {
+  width: 100%;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+}
 
-    .emailInput {
-        border-radius: 8px;
-        width: 80%;
-        height: 36px;
-        margin-bottom: 5px;
-        padding: 5px;
-    }
+.containerInput {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-    .loginBtn {
-        width: 60%;
-    }
+.emailInput {
+  border-radius: 8px;
+  width: 80%;
+  height: 36px;
+  margin-bottom: 5px;
+  padding: 5px;
+}
+
+.loginBtn {
+  width: 60%;
+}
 </style>
