4ecdcf8476c56d162417afaa950870eaebcc8d71
diasricardo23 <diasmacedo.ricardo@gmail.com>
2021-05-26 21:00:29 -0300

    Fixing card hiding about button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container1">
     <div class="containerImg">
-      <v-img alt="Logo UnB" src="../assets/LogoSemFundo_CorAlternativa.png" contain class="mt-8"/>
+      <v-img alt="Logo UnB" src="../assets/LogoSemFundo_CorAlternativa.png" max-height="300" class="mt-5"/>
     </div>
     <div class="containerButtons">
       <v-btn outlined elevation="0" color="#C5DDF7" class="loginBtn mb-5" to="/loginadm">Login Admin</v-btn>
@@ -13,22 +13,19 @@
         to="/logindonator"
       >Login Doador</v-btn>
       <v-btn outlined elevation="0" color="#C5DDF7" class="loginBtn mb-5" to="/about">Sobre a Ação</v-btn>
+    </div>
+    <div class="footer" dark padless absolute>
+      <v-card flat tile color="#547a9c" class="lighten-1 white--text text-center main_footer">
+        <v-card-text class="white--text pt-0 text_footer">
+          <p class="text-subtitle-1">Este projeto foi desenvolvido pelo  <a class="a" href="http://www.uiot.org">laboratório UIoT</a> e financiado pelo Laboratório de Tomata de Decisão <a class="a" href="http://redes.unb.br">Latitude</a></p>
+        </v-card-text>
 
-  <div class="footer" dark padless absolute>
-    <v-card flat tile color="#547a9c" class="lighten-1 white--text text-center main_footer">
-      <br>
+        <v-divider></v-divider>
 
-      <v-card-text class="white--text pt-0 text_footer">
-        <p class="text-subtitle-1">Este projeto foi desenvolvido pelo  <a class="a" href="http://www.uiot.org">laboratório UIoT</a> e financiado pelo Laboratório de Tomata de Decisão <a class="a" href="http://redes.unb.br">Latitude</a></p>
-      </v-card-text>
-
-      <v-divider></v-divider>
-
-      <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Dissemine Amor</strong>
-      </v-card-text>
-    </v-card>
-  </div>
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Dissemine Amor</strong>
+        </v-card-text>
+      </v-card>
     </div>
   </div>
 </template>
@@ -73,6 +70,7 @@
   flex: 1;
   flex-direction: column;
   align-items: center;
+  margin-bottom: 50px;
 }
 
 .containerInput {
