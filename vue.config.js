36be93498cae3e78453994da9d4b5062512119ba
diasricardo23 <diasmacedo.ricardo@gmail.com>
2021-06-09 04:17:52 -0300

    trying to change app name

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,12 @@ module.exports = {
     config.module.rules.delete('eslint');
   },
   pwa: {
-    name: 'Dissemine Amor', // <---- this is PWA name
+    manifestOptions: {
+      name: "Dissemine Amor",
+      short_name: "D Amor",
+      start_url: "./",
+      display: "standalone",
+      theme_color: "#000000",
+    },
   }
 };
